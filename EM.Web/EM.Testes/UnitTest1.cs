6dac02953ff2e2fe2564366c3d332dcb99b57de9
Joao Daniel Rosa da Silva <joao.daniel@escolarmanager.com.br>
2025-01-15 17:58:09 -0300

    Adição de um teste simples.

--- EM.Web/EM.Testes/UnitTest1.cs
+++ EM.Web/EM.Testes/UnitTest1.cs
@@ -1,15 +1,37 @@
 using EM.Dominio.Entidades;
+using EM.Dominio.Enumeradores;
+using EM.Dominio.Interfaces;
+using Moq;
 
 namespace EM.Testes;
 
 public class UnitTest1
 {
-    private long cpfInvalido = 1234567;
-    private int cpfImcompleto = 753;
+    private readonly Aluno AlunoTeste = new()
+    {
+        Matricula = 10,
+        Nome = "Jose",
+        CPF = "652.930.330-85",
+        Nascimento = new DateTime(1989, 10, 10),
+        Sexo = EnumeradorSexo.Masculino
+    };
 
     [Fact]
     public void Deve_Cadastrar_Aluno_Com_Dados_Validos()
     {
+        //Arrange
+        var mock = new Mock<IRepositorioAluno>();
 
+        //Act
+        mock.Object.Adicione(AlunoTeste);
+
+        //Assert
+        mock.Verify(m => m.Adicione(It.Is<Aluno>(
+            a => a.Matricula == AlunoTeste.Matricula
+            && a.Nome == AlunoTeste.Nome
+            && a.CPF == AlunoTeste.CPF
+            && Equals(a.Nascimento, AlunoTeste.Nascimento)
+            && Equals(a.Sexo, AlunoTeste.Sexo)
+            )), Times.Once);
     }
 }
--- EM.WebAplicacao/Program.cs
+++ EM.WebAplicacao/Program.cs
@@ -1,37 +1,28 @@
 using EM.Dominio.Interfaces;
 using EM.Repository;
 
-namespace EM.WebAplicacao
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IRepositorioAluno, RepositorioAluno>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddScoped<IRepositorioAluno, RepositorioAluno>();
-
-            var app = builder.Build();
-
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
-    }
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
