b32abcc3d00b1e14a4e6514b52d23c8e8854ce0b
joaodanielkm <joaodanielkm@gmail.com>
2025-08-25 15:54:15 -0300

    Delete EM.Web directory

--- EM.Web/Controllers/HomeController.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace EM.Web.Controllers;
-
-public class HomeController : Controller
-{
-    public IActionResult Index() => RedirectToAction("Index", "Aluno");
-}
